302052523ca571b55bc59f28f21d19969f556ddd
obidare-folu <73859076+obidare-folu@users.noreply.github.com>
2021-12-11 18:02:45 +0300

    Update BufferStack.java

--- TokenRing/src/main/java/BufferStack.java
+++ TokenRing/src/main/java/BufferStack.java
@@ -1,3 +1,7 @@
 import java.util.concurrent.ConcurrentLinkedDeque;
 
-public class BufferStack<T> extends ConcurrentLinkedDeque<T> {}
+public class BufferStack<T> extends ConcurrentLinkedDeque<T> {
+    public BufferStack () {
+        super();
+    }
+}
